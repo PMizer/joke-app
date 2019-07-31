9931bacfd34988e359b4a0b466da5dbb24f35126
pmizer <mizer.piotr@gmail.com>
2019-07-31 10:27:51 +0200

    adding xml config

--- src/main/java/com/pm/jokeapp/JokeAppApplication.java
+++ src/main/java/com/pm/jokeapp/JokeAppApplication.java
@@ -2,8 +2,10 @@ package com.pm.jokeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-configuration.xml")
 public class JokeAppApplication {
 
     public static void main(String[] args) {
